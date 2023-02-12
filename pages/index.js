7eed9b98758e3a55996b2054d64ed6e368985d41
Meek <meek10x@gmail.com>
2023-02-12 02:24:22 -0600

    adding main page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
+import styles from '../styles/styles.module.css'
+import { ideas } from '../lib/data';
+import Link from 'next/link';
+import Footer from '../components/Footer';
 
 function HomePage() {
+  console.log(ideas)
   return (
     <div>
-      Welcome to PA Mint House
+      <NavBar />
+      <Header />
+      {ideas.map((idea) => (
+        <div key={idea.name} className={styles.ideas}>
+          <Link href={idea.url}>{idea.name}</Link>
+        </div>
+      ))}
+      <Footer />
     </div>
   );
 }
