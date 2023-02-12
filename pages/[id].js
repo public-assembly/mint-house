0913c6e45a7663180821ee2f945af51e5d690e36
Meek <meek10x@gmail.com>
2023-02-12 07:44:16 -0600

    fix: removed audio demo

--- pages/[id].js
+++ pages/[id].js
@@ -11,14 +11,7 @@ const MintPage = (props) => {
       <div className={styles.grid}>
         <div className={styles.one}>Mint: {mintId}</div>
         <div className={styles.two}></div>
-        <div className={styles.three}>
-          <figure className={styles.audio}>
-            <audio controls src="/Woclott - Meek10x - Juwonmix_Master.wav">
-              Your browser does not support the
-              <code>audio</code> element.
-            </audio>
-          </figure>
-        </div>
+        <div className={styles.three}></div>
         <div className={styles.four}></div>
         <div className={styles.five}></div>
         <div className={styles.six}></div>
